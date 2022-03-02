a0d966b055f1eb650c1e17f14d27f434550547c2
Geoffery <S00198707@mail.itsligo.ie>
2022-03-02 14:21:54 +0000

    first commit

---
+++ src/app/addressbook/components/create-person/create-person.component.ts
@@ -0,0 +1,32 @@
+
+import { PeopleService } from '../../services/people.service';
+import { People } from '../../model/people.model';
+import { Component, OnInit } from '@angular/core';
+import * as uuid from 'uuid';
+import { Router } from '@angular/router';
+import{Subscription} from 'rxjs';
+import { PeopleStore } from '../../store/people.store';
+
+@Component({
+  selector: 'app-create-person',
+  templateUrl: 'create-person.component.html'
+})
+
+export class CreatePersonComponent  {
+  
+createPersonSub: Subscription;
+  router: any;
+
+  constructor(private store: PeopleStore, private personService:Router) { }
+
+
+  onSubmit(submittedForm: { value: { lastname: any; firstname: any; phone: any; }; invalid: any; }) {
+    if (submittedForm.invalid) {
+      return;
+    }
+    const people: People = {id: uuid.v4(), lastname: submittedForm.value.lastname,firstname: submittedForm.value.firstname, phone: submittedForm.value.phone};
+    this.createPersonSub = this.personService.createPerson(people).subscribe(result => {
+      this.router.navigateByUrl('/people');
+    });
+  }
+}
